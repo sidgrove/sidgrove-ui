62535e213cf16997a867f5fec6e6b8d7dfe52804
David Sellick <dave@sidgrove.com>
2026-04-17 15:57:33 +0100

    feat: barrel export for @sidgrove/ui

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// src/index.ts
+export * from "./components/ui"
+export { AmbientBackground } from "./components/AmbientBackground"
+export { LoadingScreen } from "./components/LoadingScreen"
+export { AppHeader } from "./components/AppHeader"
+export { LoginShell } from "./components/LoginShell"
+export { cn } from "./lib/utils"
+export { T, cssVars, type BrandToken } from "./tokens"
